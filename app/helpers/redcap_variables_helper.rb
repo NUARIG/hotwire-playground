e96d38be383c24c347653094ba64853956dabf7c
Michael Gurley <m-gurley@northwestern.edu>
2021-11-05 10:31:10 -0400

    Show/hide columns based on map type for redcap variable child map.

--- app/helpers/redcap_variables_helper.rb
+++ app/helpers/redcap_variables_helper.rb
@@ -27,14 +27,6 @@ module RedcapVariablesHelper
     end
   end
 
-  def hide_redcap_variable_choice_map_concept_id(redcap_variable_choice)
-    if redcap_variable_choice.curation_status == Redcap2omop::RedcapVariableChoice::REDCAP_VARIABLE_CHOICE_CURATION_STATUS_MAPPED
-      ''
-    else
-      'hide'
-    end
-  end
-
   def hide_redcap_variable_child_maps(redcap_variable)
     if redcap_variable.redcap_variable_map
       if redcap_variable.redcap_variable_map.map_type == Redcap2omop::RedcapVariableMap::REDCAP_VARIABLE_MAP_MAP_TYPE_OMOP_CONCEPT
@@ -42,6 +34,38 @@ module RedcapVariablesHelper
       else
         'hide'
       end
+    else
+      'hide'
+    end
+  end
+
+  def hide_redcap_variable_child_map_concept_id(redcap_variable_child_map)
+    if redcap_variable_child_map.map_type == Redcap2omop::RedcapVariableChildMap::REDCAP_VARIABLE_CHILD_MAP_MAP_TYPE_OMOP_CONCEPT
+      ''
+    else
+      'hide'
+    end
+  end
+
+  def hide_redcap_variable_child_map_redcap_variable_id(redcap_variable_child_map)
+    if redcap_variable_child_map.map_type == Redcap2omop::RedcapVariableChildMap::REDCAP_VARIABLE_CHILD_MAP_MAP_TYPE_REDCAP_VARIABLE
+      ''
+    else
+      'hide'
+    end
+  end
+
+  def hide_redcap_variable_child_map_redcap_derived_date_id(redcap_variable_child_map)
+    if redcap_variable_child_map.map_type == Redcap2omop::RedcapVariableChildMap::REDCAP_VARIABLE_CHILD_MAP_MAP_TYPE_REDCAP_DERIVED_DATE
+      ''
+    else
+      'hide'
+    end
+  end
+
+  def hide_redcap_variable_choice_map_concept_id(redcap_variable_choice)
+    if redcap_variable_choice.curation_status == Redcap2omop::RedcapVariableChoice::REDCAP_VARIABLE_CHOICE_CURATION_STATUS_MAPPED
+      ''
     else
       'hide'
     end
